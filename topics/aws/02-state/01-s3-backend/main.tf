5161605418800ecfaf1d8f78952212fc0a3262b5
Nico Serfontein <nserfontein@gmail.com>
2018-07-04 19:53:03 +0200

    Update

---
+++ topics/aws/02-state/01-s3-backend/main.tf
@@ -0,0 +1,36 @@
+provider "aws" {
+  region = "eu-west-1"
+}
+
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "terraform-up-and-running-state-nico"
+  versioning {
+    enabled = true
+  }
+  lifecycle {
+    //prevent_destroy = true
+  }
+}
+
+terraform {
+  backend "s3" {
+    bucket = "terraform-up-and-running-state-nico"
+    key = "global/s3/terraform.tfstate"
+    region = "eu-west-1"
+    encrypt = true
+  }
+}
+
+data "terraform_remote_state" "network" {
+  backend = "s3"
+  config {
+    bucket = "terraform-up-and-running-state-nico"
+    key = "global/s3/terraform.tfstate"
+    region = "eu-west-1"
+    encrypt = true
+  }
+}
+
+output "s3_bucket_arn" {
+  value = "${aws_s3_bucket.terraform_state.arn}"
+}
