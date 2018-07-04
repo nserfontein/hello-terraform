df39766688445cfbba93e5bbcd655f2e3e9b10c1
Nico Serfontein <nserfontein@gmail.com>
2018-07-04 18:15:41 +0200

    Update

---
+++ topics/aws/01-basics/03-clustered-web-servers/main.tf
@@ -0,0 +1,62 @@
+provider "aws" {
+  region = "eu-west-1"
+}
+
+resource "aws_launch_configuration" "example" {
+  image_id = "ami-58d7e821"
+  instance_type = "t2.micro"
+  security_groups = [
+    "${aws_security_group.instance.id}"]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World" > index.html
+              nohup busybox httpd -f -p "${var.server_port}" &
+              EOF
+
+  lifecycle {
+    create_before_destroy = true
+  }
+
+}
+
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"
+
+  ingress {
+    from_port = "${var.server_port}"
+    to_port = "${var.server_port}"
+    protocol = "tcp"
+    cidr_blocks = [
+      "0.0.0.0/0"
+    ]
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+
+}
+
+variable "server_port" {
+  description = "The port the server will use for HTTP requests"
+  default = 8080
+}
+
+resource "aws_autoscaling_group" "example" {
+  launch_configuration = "${aws_launch_configuration.example.id}"
+  availability_zones = [
+    "${data.aws_availability_zones.all.names}"
+  ]
+
+  min_size = 2
+  max_size = 10
+
+  tag {
+    key = "Name"
+    value = "terraform-asg-example"
+    propagate_at_launch = true
+  }
+}
+
+data "aws_availability_zones" "all" {}
