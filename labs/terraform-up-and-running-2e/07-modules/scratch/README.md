4fde2be992e910f9b4a705c5648afdbdf4f326e8
Nico Serfontein <nserfontein@ebay.com>
2019-09-10 14:02:30 +0200

    Update

---
+++ labs/terraform-up-and-running-2e/07-modules/scratch/README.md
@@ -0,0 +1,10 @@
+# Steps
+```shell script
+cd stage/services/webserver-cluster
+terraform init
+terraform apply
+
+```
+
+# NOTES:
+- Prod environment is work in progress, see example code
---
+++ labs/terraform-up-and-running-2e/07-modules/scratch/prod/services/webserver-cluster/main.tf
@@ -0,0 +1,15 @@
+provider "aws" {
+  region = "us-east-2"
+}
+
+module "webserver_cluster" {
+  source = "../../../modules/services/webserver-cluster"
+
+  cluster_name           = "webservers-prod"
+  db_remote_state_bucket = "terraform-up-and-running-state-nserfontein"
+  db_remote_state_key    = "prod/data-stores/mysql/terraform.tfstate"
+
+  instance_type = "m4.large"
+  min_size      = 2
+  max_size      = 10
+}
