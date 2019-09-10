034ba5c0abc958662078a25952149a62d453202b
Nico Serfontein <nserfontein@ebay.com>
2019-09-10 11:47:05 +0200

    Update

--- labs/terraform-up-and-running-2e/05-state-isolation-via-workspaces/scratch/README.md
+++ labs/terraform-up-and-running-2e/05-state-isolation-via-workspaces/scratch/README.md
@@ -1,5 +1,35 @@
+# NOTE:
+- Good for isolating setups in same environment
+- Not suitable to isolate different environments (dev, qa, prod) because it is very easy to using the wrong workspace (destroying production by mistake)
+
+# Steps
 ```shell script
 source .secrets/aws-access.sh
 
+terraform init
+terraform apply
+
+terraform workspace show
+# will be 'default'
+
+terraform workspace new example1
+terraform plan
+terraform apply # This will create a new instance in example1 workspace
+
+terraform workspace new example2
+terraform plan
+terraform apply # This will create a new instance in example2 workspace
+```
+
+# Cleanup
+```shell script
+terraform workspace select example2
+terraform destroy
+
+terraform workspace select example1
+terraform destroy
+
+terraform workspace select default
+terraform destroy
 ```
 
--- labs/terraform-up-and-running-2e/05-state-isolation-via-workspaces/scratch/main.tf
+++ labs/terraform-up-and-running-2e/05-state-isolation-via-workspaces/scratch/main.tf
@@ -2,51 +2,46 @@ provider "aws" {
   region = "us-east-2"
 }
 
-resource "aws_instance" "example" {
-  ami           = "ami-0c55b159cbfafe1f0"
-  instance_type = "t2.micro"
-}
-
-resource "aws_s3_bucket" "terraform_state" {
-  bucket = "terraform-up-and-running-state-nserfontein"
-
-  # Prevent accidental deletion of this S3 bucket
-  lifecycle {
-    prevent_destroy = true
-  }
-
-  # Enable versioning so we can see the full revision history of our
-  # state files
-  versioning {
-    enabled = true
-  }
-
-  # Enable server-side encryption by default
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
-    }
-  }
-}
-
-resource "aws_dynamodb_table" "terraform_locks" {
-  name         = "terraform-up-and-running-locks"
-  billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "LockID"
-
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
-}
+//resource "aws_s3_bucket" "terraform_state" {
+//  bucket = "terraform-up-and-running-state-nserfontein"
+//
+//  # Prevent accidental deletion of this S3 bucket
+//  lifecycle {
+//    prevent_destroy = true
+//  }
+//
+//  # Enable versioning so we can see the full revision history of our
+//  # state files
+//  versioning {
+//    enabled = true
+//  }
+//
+//  # Enable server-side encryption by default
+//  server_side_encryption_configuration {
+//    rule {
+//      apply_server_side_encryption_by_default {
+//        sse_algorithm = "AES256"
+//      }
+//    }
+//  }
+//}
+//
+//resource "aws_dynamodb_table" "terraform_locks" {
+//  name         = "terraform-up-and-running-locks"
+//  billing_mode = "PAY_PER_REQUEST"
+//  hash_key     = "LockID"
+//
+//  attribute {
+//    name = "LockID"
+//    type = "S"
+//  }
+//}
 
 terraform {
   backend "s3" {
     # Replace this with your bucket name!
     bucket         = "terraform-up-and-running-state-nserfontein"
-    key            = "global/s3/terraform.tfstate"
+    key            = "workspaces-example/terraform.tfstate"
     region         = "us-east-2"
 
     # Replace this with your DynamoDB table name!
@@ -55,12 +50,17 @@ terraform {
   }
 }
 
-output "s3_bucket_arn" {
-  value       = aws_s3_bucket.terraform_state.arn
-  description = "The ARN of the S3 bucket"
+resource "aws_instance" "example" {
+  ami           = "ami-0c55b159cbfafe1f0"
+  instance_type = "t2.micro"
 }
 
-output "dynamodb_table_name" {
-  value       = aws_dynamodb_table.terraform_locks.name
-  description = "The name of the DynamoDB table"
-}
+//output "s3_bucket_arn" {
+//  value       = aws_s3_bucket.terraform_state.arn
+//  description = "The ARN of the S3 bucket"
+//}
+//
+//output "dynamodb_table_name" {
+//  value       = aws_dynamodb_table.terraform_locks.name
+//  description = "The name of the DynamoDB table"
+//}
